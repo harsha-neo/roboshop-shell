f2f1e0c7fa13795205dd0f703fa0bdc3e09a65a1
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-16 21:23:06 +0100

    robo mongo

--- catalogue.sh
+++ catalogue.sh
@@ -14,4 +14,4 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
 dnf install mongodb-mongosh -y
-mongosh --localhost </app/db/master-data.js
+mongosh --host localhost </app/db/master-data.js
