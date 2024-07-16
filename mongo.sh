0f2bbb6ed4d014f665862e2b9c2f9e7cafa0a0e2
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-16 22:17:16 +0100

    robo update conf file

--- mongo.sh
+++ mongo.sh
@@ -2,5 +2,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-# edit conf file
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
