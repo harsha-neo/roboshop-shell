af5493350a31fa6cab662e235a9a1e12b4b30656
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-18 21:28:26 +0100

    robo cart

--- redis.sh
+++ redis.sh
@@ -1,8 +1,7 @@
 dnf module disable redis -y
 dnf module enable redis:7 -y
 dnf install redis -y
-sed -i -e '/^bind/ s/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
-sed -i -e '/protected-mode/ c /protected-mode no' /etc/redis/redis.conf
+
 systemctl enable redis
 systemctl start redis
 systemctl restart redis
