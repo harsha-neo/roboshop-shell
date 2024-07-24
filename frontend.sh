e7e688a0f533ee0cf64ba415e15abffd0c7737e2
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-24 20:49:35 +0100

    dispatch setup

--- frontend.sh
+++ frontend.sh
@@ -3,35 +3,35 @@ source common.sh
 component=frontend
 
 PRINT Disable nginx default version
-dnf module disable nginx -y
+dnf module disable nginx -y >>$LOG_FILE
 STAT $?
 
 PRINT enable nginx version24
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y >>$LOG_FILE
 STAT $?
 
 PRINT installing nginx
-dnf install nginx -y
+dnf install nginx -y >>$LOG_FILE
 STAT $?
 
 PRINT copy nginx
-cp nginx.conf /etc/nginx/nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf >>$LOG_FILE
 STAT $?
 
 PRINT Remove the old content
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* >>$LOG_FILE
 STAT $?
 
 PRINT download the application
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >>$LOG_FILE
 STAT $?
 
 PRINT ectract the application
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html >>$LOG_FILE
+unzip /tmp/frontend.zip >>$LOG_FILE
 STAT $?
 
 PRINT restart nginx
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx >>$LOG_FILE
+systemctl restart nginx >>$LOG_FILE
 STAT $?
