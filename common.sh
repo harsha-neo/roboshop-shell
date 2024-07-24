d4b9002d55cd9375218fafbf72e048f93aba7071
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-24 20:35:15 +0100

    dispatch setup

---
+++ common.sh
@@ -0,0 +1,21 @@
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+
+PRINT() {
+  echo &>>$LOG_FILE
+  echo &>>$LOG_FILE
+  echo "############### $* #############" &>>$LOG_FILE
+  echo $*
+}
+
+STAT () {
+  if [$1 -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    echo
+    echo "refer the log file for more information : file path : ${LOG_FILE}"
+    exit $1
+    fi
+}
+
--- frontend.sh
+++ frontend.sh
@@ -1,11 +1,37 @@
 #frontend
+source common.sh
+component=frontend
+
+PRINT Disable nginx default version
 dnf module disable nginx -y
+STAT $?
+
+PRINT enable nginx version24
 dnf module enable nginx:1.24 -y
+STAT $?
+
+PRINT installing nginx
 dnf install nginx -y
+STAT $?
+
+PRINT copy nginx
 cp nginx.conf /etc/nginx/nginx.conf
+STAT $?
+
+PRINT Remove the old content
 rm -rf /usr/share/nginx/html/*
+STAT $?
+
+PRINT download the application
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+STAT $?
+
+PRINT ectract the application
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+STAT $?
+
+PRINT restart nginx
 systemctl enable nginx
 systemctl restart nginx
+STAT $?
