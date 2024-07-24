ba77da9be3e6b4dea52f3caf83abbde7dac5c97d
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-24 20:55:28 +0100

    dispatch setup

--- frontend.sh
+++ frontend.sh
@@ -1,33 +1,24 @@
 #frontend
-PRINT Disable nginx default version
-dnf module disable nginx -y >>$LOG_FILE
+dnf module disable nginx -y
 
 
-PRINT enable nginx version24
-dnf module enable nginx:1.24 -y >>$LOG_FILE
+dnf module enable nginx:1.24 -y
+
+dnf install nginx -y
+
+cp nginx.conf /etc/nginx/nginx.conf
+rm -rf /usr/share/nginx/html/*
 
 
-PRINT installing nginx
-dnf install nginx -y >>$LOG_FILE
-
-
-PRINT copy nginx
-cp nginx.conf /etc/nginx/nginx.conf >>$LOG_FILE
-
-
-PRINT Remove the old content
-rm -rf /usr/share/nginx/html/* >>$LOG_FILE
-
-
-PRINT download the application
+#PRINT download the application
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip >>$LOG_FILE
 
 
-PRINT ectract the application
+#PRINT ectract the application
 cd /usr/share/nginx/html >>$LOG_FILE
 unzip /tmp/frontend.zip >>$LOG_FILE
 
 
-PRINT restart nginx
+#PRINT restart nginx
 systemctl enable nginx >>$LOG_FILE
 systemctl restart nginx >>$LOG_FILE
