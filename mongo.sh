bbd20ad811138323896d6b30646f013e4c46e0c3
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-25 10:08:57 +0100

    dispatch setup

--- mongo.sh
+++ mongo.sh
@@ -1,6 +1,22 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl restart mongod
+source common.sh
+
+PRINT copy mongo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>LOG_FILE
+STAT$?
+
+PRINT installing mongo
+dnf install mongodb-org -y &>>LOG_FILE
+STAT$?
+
+PRINT enable mongo
+systemctl enable mongod &>>LOG_FILE
+systemctl start mongod &>>LOG_FILE
+STAT$?
+
+PRINT mongo sed
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>LOG_FILE
+STAT$?
+
+PRINT restart mongo
+systemctl restart mongod &>>LOG_FILE
+STAT$?
