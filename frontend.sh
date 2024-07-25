4058e7a0b217f059b286404db235b08848d92cba
saiharsha <saiharshavardhan.batta@gmail.com>
2024-07-25 09:41:31 +0100

    dispatch setup

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,24 @@
 #frontend
-dnf module disable nginx -y &>>$LOG_FILE
+dnf module disable nginx -y &>>LOG_FILE
 
 
-dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>LOG_FILE
 
-dnf install nginx -y &>>$LOG_FILE
+dnf install nginx -y &>>LOG_FILE
 
-cp nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+cp nginx.conf /etc/nginx/nginx.conf &>>LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>LOG_FILE
 
 
 #PRINT download the application
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>LOG_FILE
 
 
 #PRINT ectract the application
-cd /usr/share/nginx/html &>>$LOG_FILE
-unzip /tmp/frontend.zip &>>$LOG_FILE
+cd /usr/share/nginx/html &>>LOG_FILE
+unzip /tmp/frontend.zip &>>LOG_FILE
 
 
 #PRINT restart nginx
-systemctl enable nginx &>>$LOG_FILE
-systemctl restart nginx &>>$LOG_FILE
+systemctl enable nginx &>>LOG_FILE
+systemctl restart nginx &>>LOG_FILE
